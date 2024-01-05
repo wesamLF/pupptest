888cf09e034aabd12bd1f8682d42e74531d9566c
wesam l <wesamlafi390@gmail.com>
2024-01-05 16:36:52 +0300

    ppp

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -5,32 +5,25 @@ const scrapeLogic = async (res) => {
   console.error("scraper running");
 
   const browser = await puppeteer.launch({
-    headless:"new",
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--no-zygote",
-    ],
-    // executablePath:
-    //   process.env.NODE_ENV === "production"
-    //     ? process.env.PUPPETEER_EXECUTABLE_PATH
-    //     : puppeteer.executablePath(),
+    executablePath: '/usr/bin/chromium-browser',
+    headless: 'new',
+    slowMo: undefined
   });
   try {
-    
+
     const page = await browser.newPage()
     await page.goto("https://webscraper.io/test-sites/e-commerce/allinone", {
-        waitUntil: "networkidle2",
+      waitUntil: "networkidle2",
     })
     const data = await page.evaluate(() => {
 
-           return document.querySelector("h4.float-end.price.card-title.pull-right").textContent
-            
+      return document.querySelector("h4.float-end.price.card-title.pull-right").textContent
+
 
     })
     res.send(data)
     await browser.close()
-    
+
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
